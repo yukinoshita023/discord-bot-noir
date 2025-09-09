7767e95f1334bba9253cfe0726842813ce125471
taisei023 <tm.lovin8@gmail.com>
2025-09-09 22:24:05 +0900

    #50 helloコマンドを修正

--- commands/hello.py
+++ commands/hello.py
@@ -8,13 +8,13 @@ def has_role(role_id):
         if role:
             return True
         else:
-            await interaction.response.send_message("あなたにはこのコマンドを実行する権限がありません。", ephemeral=True)
+            await interaction.response.send_message("あなたにはこのコマンドを実行する権限がありません", ephemeral=True)
             return False
     return discord.app_commands.check(predicate)
 
 async def setup(bot):
     
-    @bot.tree.command(name="hello", description="このコマンドは「やっほー！」と返します")
+    @bot.tree.command(name="hello", description="このコマンドによってノワールの生存確認ができます")
     @has_role(ADMIN_ROLE_ID)
     async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message("やっほー！")
+        await interaction.response.send_message("職務遂行中です！")
